9baf3cc00619487f9aba8b894800baeba58b196d
vàng hoa <78696365+vanghoa@users.noreply.github.com>
2024-07-07 14:42:54 +0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The footer
+ *
+ * @package Bathe
+ */
+
+?>
+</main>
+
+	<?php get_sidebar(); ?>
+</div>
+
+<footer class="footer">
+	<p class="copyright">© 2020 the contributors of Bathe under the GPLv2 license.</p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
